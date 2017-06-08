e6ba70b4d3f429aa65a375f1aa0c8f4e3ee3ca2d
jiaoqiqi <1176932165@qq.com>
2017-06-08 12:51:16 +0800

    write myself to respose

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,35 +1,21 @@
 const http = require('http');
-const fs = require('fs');
 const url = require('url');
+const fs = require('fs');
 
+http.createServer((req, res) => {
+    const pathname = url.parse(req.url).pathname;
 
-// 创建服务器
-http.createServer(  (request, response)=> {
-    // 解析请求，包括文件名
-    var pathname = url.parse(request.url).pathname;
-
-    // 输出请求的文件名
-    console.log("Request for " + pathname + " received.");
-
-    // 从文件系统中读取请求的文件内容
-    fs.readFile(pathname.substr(1),  (err, data)=> {
+    fs.readFile(pathname.substr(1), (err, data) => {
         if (err) {
             console.log(err);
-            // HTTP 状态码: 404 : NOT FOUND
-            // Content Type: text/plain
-            response.writeHead(404, {'Content-Type': 'text/html'});
-        }else{
-            // HTTP 状态码: 200 : OK
-            // Content Type: text/plain
-            response.writeHead(200, {'Content-Type': 'text/html'});
-
-            // 响应文件内容
-            response.write(data.toString());
+            res.writeHead(404, {'Content-Type': 'text/html'});
         }
-        //  发送响应数据
-        response.end();
+        else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data.toString());
+        }
+        res.end()
     });
 }).listen(8081);
 
-// 控制台会输出以下信息
-console.log('Server running at http://127.0.0.1:8081/');
+console.log("Server running at http://127.0.0.1:8081/");
