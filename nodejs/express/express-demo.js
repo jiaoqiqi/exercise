a55a3e46236c27656af3dc2f4fb5b31398e27984
jiaoqiqi <1176932165@qq.com>
2017-06-10 18:40:33 +0800

    use static public to load inages
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- nodejs/express/express-demo.js
+++ nodejs/express/express-demo.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+//  访问 http://127.0.0.1:8081/images/1.jpg
+
+
 app.get('/', (req, res) => {
     res.send("hello GET");
 });
@@ -22,3 +26,4 @@ const server = app.listen(8081, () => {
     const port = server.address().port;
     console.log("应用实例，访问地址为 http://%s:%s", host, port);
 });
+
