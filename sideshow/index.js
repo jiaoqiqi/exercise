bb27259dda8d4767a503813ffc87f03e01d98793
jiaoqiqi <1176932165@qq.com>
2017-10-16 21:14:56 +0800

    不管怎么按都会有图片
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- sideshow/index.js
+++ sideshow/index.js
@@ -8,13 +8,25 @@ window.onload = function() {
         //且style.left获取的是字符串，需要用parseInt()取整转化为数字。
         var newLeft = parseInt(list.style.left) + offset;
         list.style.left = newLeft + 'px';
-        console.log(list.style.left)
+
+
+        // 之前一直点击右箭头 ，会出现空白，而且，不能回到第一张图片。要点击左箭头才能回到第一张图片。
+        // 因为没有设置偏移量的范围，超过范围就么有图片啦啦啦啦
+        // F12调试当看到left值小于3600时，因为没有第8张图片就出现空白，所以这里我们需要对偏移量做一个判断。
+        if(newLeft<-3000){
+            list.style.left = -600 + 'px';
+        }
+        if(newLeft>-600){
+            list.style.left = -3000 + 'px';
+        }
     }
 
     prev.onclick = function() {
         animate(600);
+        // 按前面一张图位置加一张图的宽度即600px
     }
     next.onclick = function() {
         animate(-600);
     }
+
 }
