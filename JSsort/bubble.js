d765c594b3ed4a8d131f4c5e0e557cfe21cadfb7
jiaoqiqi <1176932165@qq.com>
2018-03-08 10:50:13 +0800

    save time
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- JSsort/bubble.js
+++ JSsort/bubble.js
@@ -23,7 +23,7 @@ function bubble1(arr) {
     var i = arr.length - 1;
     while (i > 0) {
         var pos = 0;
-        for (var j = 0;  j < i; j++) {
+        for (var j = 0; j < i; j++) {
             if (arr[i] > arr[i + 1]) {
                 pos = j;
                 var tmp = arr[j];
@@ -38,3 +38,33 @@ function bubble1(arr) {
 }
 
 console.log(bubble1(arr));
+
+
+// 解决一次只能找到最大值或最小值的问题,在每趟排序中进行正向反向两遍冒泡
+function bubble2(arr) {
+    var low = 0;
+    var high = arr.length - 1;
+    var tmp, j;
+    while (low < high) {
+        for (j = low; j < high; j++) {
+            if (arr[j] > arr[j + 1]) {
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+            --high;
+        }
+        for (j = high; j > low; --j) {
+            if(arr[j] < arr[j-1]){
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+            ++low;
+        }
+        return arr;
+
+    }
+
+}
+console.log(bubble2(arr));
