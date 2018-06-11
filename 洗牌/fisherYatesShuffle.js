3dfd4cb444f17c4b0908ea1b5bc0e4c56d956f00
jiaoqiqi <1176932165@qq.com>
2018-06-11 15:33:25 +0800

    try mock
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- 洗牌/fisherYatesShuffle.js
+++ 洗牌/fisherYatesShuffle.js
@@ -1,10 +1,11 @@
 //打乱数组
+//从数组最后一个开始  随机与前面的元素进行交换
 function shuffle(array) {
 
     var _array = array.concat();
     for (var i = _array.length; i--; ) {
-        var j = ~~(Math.random() * (i + 1));
-        [_array[i],_array[j]] = [_array[j],_array[i]]
+        var j = ~~(Math.random() * (i + 1));  // 向下取整
+        [_array[i],_array[j]] = [_array[j],_array[i]]   //交换两个元素
     }
 
     return _array;
